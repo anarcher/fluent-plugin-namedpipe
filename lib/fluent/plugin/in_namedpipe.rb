dd9b3336bebbb3fe13b18d31f5e589fede683621
msshin <msshin@enswer.net>
2014-10-29 19:05:07 +0900

    It's works but..

--- lib/fluent/plugin/in_namedpipe.rb
+++ lib/fluent/plugin/in_namedpipe.rb
@@ -13,25 +13,29 @@ module Fluent
 
         config_param :path, :string
         config_param :tag , :string
+        #TODO: Not use yet
+        #config_param :receive_interval, :time, :default => 1
 
         def configure(conf)
             super
 
-            if !File.exists(@path)
+            if !File.exists?(@path)
                 raise ConfigError,"File not found #{@path}"
             end
 
+            if @tag.nil? 
+                raise ConfigError,"tag is empty"
+            end
+
             configure_parser(conf)
-            configure_tag
-
         end
-
 
         def configure_parser(conf)
             @parser = TextParser.new
             @parser.configure(conf)
         end
 
+        #TODO: Not yet used
         def configure_tag
             if @tag.index('*')
                 @tag_prefix, @tag_suffix = @tag.split('*')
@@ -49,25 +53,40 @@ module Fluent
 
         def start
             super
-            @pipe = open(@path,"r+")
+            @pipe = open(@path,"r")
+            @finished = false
+            @thread = Thread.new(&method(:run))
         end
 
         def run
-            loop do
-                line = @pipe.gets
-                time, record = parse_line(line)
-                log.debug("Line: #{time} #{record}")
-                if time && record
-                    Engine.emit(@msg,time,record)
-                else
-                    log.warn "Pattern not match: #{line.inspect}"
+            until @finished
+                begin
+                    lines = @pipe.gets
+                    if lines.nil?
+                        next
+                    end
+
+                    lines = lines.split("\n")
+                    lines.each { |line| 
+                        time, record = parse_line(line)
+                        if time && record
+                            Engine.emit(@tag,time,record)
+                        else
+                            log.warn "Pattern not match: #{line.inspect}"
+                        end
+                    }
+                rescue
+                    log.error "unexpected error", :error=>$!.to_s
+                    log.error_backtrace
                 end
             end
         end
 
         def shutdown
             super
-            @pipe.close()
+            @finished = true
+            @thread.join
+            @pipe.close
         end
     end
 end
